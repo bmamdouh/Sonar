cb17f4558d54c9fb119b761ed89e7668ca94ffd4
bmamdouh <bassam.mmdouh@gmail.com>
2018-12-04 14:30:46 -0500

    First commit

---
+++ src/sonartest/myApp.java
@@ -0,0 +1,11 @@
+package sonartest;
+
+public class myApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sonarTest st = new sonarTest();
+		st.doAct();
+	}
+
+}
---
+++ src/sonartest/sonarTest.java
@@ -0,0 +1,7 @@
+package sonartest;
+
+public class sonarTest {
+ public int  doAct() {
+	 return 1;
+ }
+}
